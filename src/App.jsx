b60db13b11f37748905fb4b0479e7976a44bba1a
minseo <kmssally05@gmail.com>
2024-10-28 01:47:56 +0900

    fix : 라우팅 관련 경로 수정 - App.jsx 라우팅 경로 추가 - Posting.jsx로 UI 구현 - Landing 페이지는 일단 링크만 연결해둠 ref : #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Posting from "./pages/recruitment/Posting";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/posting" element={<Posting />} />
       </Routes>
     </BrowserRouter>
   );
