0fa2824263b1f5dd0965dcf9dd8a5e1631109758
odukong <ohbin1017@gmail.com>
2024-11-08 14:57:01 +0900

    ✨ feat : 홈/검색 알바리스트 API 연결
    - 홈 알바리스트 불러오기 API
    - 키워드 검색 알바리스트 API
    - 데이터 불러오는 과정에서의 로딩 스피너 구현(react-spinners)
    
    ref : #16

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
 import { verifyCEOAPI, signupUserAPI, signupCEOAPI } from "./signupAPI";
 import { loginAPI } from "./loginAPI";
+import { postListAPI, searchPostListAPI } from "./homeAPI";
 
 export {
   verifyCEOAPI, // 사업자 인증 API
   signupUserAPI, // 일반유저 회원가입 API
   signupCEOAPI, // 사업자유저 회원가입 API
   loginAPI, // 로그인 API
+  postListAPI, // 알바리스트 불러오기(홈) API
+  searchPostListAPI, // 알바리스트 불러오기 (검색) API
 };
