fd75dfa7fa8a4d7ffed89fca0ef2d976ed07b3ad
odukong <ohbin1017@gmail.com>
2024-11-09 02:50:20 +0900

    hotfix : privateAxios 수정 - 인자 추가하여 axios 인스턴스 추가

--- src/utils/customAxios.js
+++ src/utils/customAxios.js
@@ -10,6 +10,7 @@ const createPublicAxios = () => {
 };
 const publicAxios = createPublicAxios();
 
+// 토큰 재발급 함수 작성..
 // Axios with token
 const createPrivateAxios = (accessToken, dispatch) => {
   const instance = axios.create({
@@ -19,12 +20,32 @@ const createPrivateAxios = (accessToken, dispatch) => {
     },
   });
 
-  // 리프레쉬 토큰 관련 코드 추가
-  instance.interceptors.request.use((response) => {
-    return response;
-  });
+  // [요청 설정] 모든 요청의 헤더에 토큰 넣어 보내기
+  instance.interceptors.request.use(
+    (config) => {
+      // refresh 토큰 요청(토큰 재발급) vs 그 외 요청
+      return config;
+    },
+    (error) => {
+      // 요청 실패 시 에러 처리
+      return Promise.reject(error);
+    }
+  );
+
+  // [응답 설정]
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const { config, response } = error;
+      // 토큰 재발급 요청하기
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
 };
 
-const privateAxios = createPrivateAxios();
+const privateAxios = (accessToken, dispatch) =>
+  createPrivateAxios(accessToken, dispatch);
 
 export { publicAxios, privateAxios };
